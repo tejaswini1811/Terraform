3a7a9eb56e31ada5044834c75553af7c6e56a998
tejaswini1811 <tejaswinibokka18@gmail.com>
2023-04-07 11:53:20 +0530

    updated

--- Ansible/variable.tf
+++ Ansible/variable.tf
@@ -1,11 +1,15 @@
 variable "region" {
   type = string
+  default = "us-west-2"
 }
+
 variable "availability_zone" {
   type = list(string)
+  default = ["us-west-2a", "us-west-2b"]
 }
 variable "sg_name" {
   type = string
+  default = "allclear"
 }
 variable "instances_info" {
   type = map(object({
@@ -14,7 +18,16 @@ variable "instances_info" {
     user_data         = string
     availability_zone = string
   }))
+  default = {
+   "ansible" = {
+    instance_type     = "t2.micro"
+    key_name          = "impkey"
+    availability_zone = "us-west-2a"
+    user_data         = "ansible.sh"
+  }
+  }
 }
 variable "null" {
   type = string
+  default = "11"
 }
