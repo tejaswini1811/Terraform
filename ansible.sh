2f91ed12c5cdf53094568646fbae07159c302d8d
tejaswini1811 <tejaswinibokka18@gmail.com>
2023-04-11 11:20:58 +0530

    added

---
+++ ansible.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+ansible --version
+seq 18
